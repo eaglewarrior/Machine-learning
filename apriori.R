994407e25012f9bdfd4e5a28a0a012e2218591ed
RINKI NAG <rinkinag24@gmail.com>
2018-09-08 12:05:09 +0530

    Update apriori.R

--- apriori.R
+++ apriori.R
@@ -3,8 +3,8 @@
 # Data Preprocessing
 # install.packages('arules')
 library(arules)
-dataset = read.csv('/home/fractaluser/Desktop/Apriori-R/Apriori/Market_Basket_Optimisation.csv', header = FALSE)
-dataset = read.transactions('/home/fractaluser/Desktop/Apriori-R/Apriori/Market_Basket_Optimisation.csv', sep = ',', rm.duplicates = TRUE)
+dataset = read.csv('...../Apriori-R/Apriori/Market_Basket_Optimisation.csv', header = FALSE)
+dataset = read.transactions('/......./Apriori-R/Apriori/Market_Basket_Optimisation.csv', sep = ',', rm.duplicates = TRUE)
 summary(dataset)
 itemFrequencyPlot(dataset, topN = 10)
 
