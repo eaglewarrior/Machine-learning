1bec71b4e9f4cf8e5fdbb08a1594e777df88c52a
RINKI NAG <rinkinag24@gmail.com>
2018-09-08 12:03:46 +0530

    apriori code for market basket analysis

---
+++ apriori.R
@@ -0,0 +1,15 @@
+# Apriori
+
+# Data Preprocessing
+# install.packages('arules')
+library(arules)
+dataset = read.csv('/home/fractaluser/Desktop/Apriori-R/Apriori/Market_Basket_Optimisation.csv', header = FALSE)
+dataset = read.transactions('/home/fractaluser/Desktop/Apriori-R/Apriori/Market_Basket_Optimisation.csv', sep = ',', rm.duplicates = TRUE)
+summary(dataset)
+itemFrequencyPlot(dataset, topN = 10)
+
+# Training Apriori on the dataset
+rules = apriori(data = dataset, parameter = list(support = 0.004, confidence = 0.2))
+
+# Visualising the results
+inspect(sort(rules, by = 'lift')[1:10])
