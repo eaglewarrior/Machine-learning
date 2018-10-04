63bd02e89ad58a98b090f50d68f1b0f63edb3bd2
RINKI NAG <rinkinag24@gmail.com>
2018-10-04 18:29:27 +0530

    Update pca.py

--- pca.py
+++ pca.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # Importing the dataset
-dataset = pd.read_csv('/home/fractaluser/Desktop/ml/PCA/Wine.csv')
+dataset = pd.read_csv('..../ml/PCA/Wine.csv')
 X = dataset.iloc[:, 0:13].values
 y = dataset.iloc[:, 13].values
 
