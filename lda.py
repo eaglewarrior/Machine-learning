e192b2e003dbdbf3ffc5f95dda96e9c5d9094e55
RINKI NAG <rinkinag24@gmail.com>
2018-10-04 18:30:51 +0530

    Update lda.py

--- lda.py
+++ lda.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # Importing the dataset
-dataset = pd.read_csv('/home/fractaluser/Desktop/LDA/LDA/Wine.csv')
+dataset = pd.read_csv('.../LDA/LDA/Wine.csv')
 X = dataset.iloc[:, 0:13].values
 y = dataset.iloc[:, 13].values
 
