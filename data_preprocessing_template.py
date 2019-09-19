07fcbe19a88a248759ce0a610879c5acdac7b0e7
RINKI NAG <rinkinag24@gmail.com>
2019-09-19 21:35:41 +0530

    Update data_preprocessing_template.py

--- data_preprocessing_template.py
+++ data_preprocessing_template.py
@@ -31,7 +31,7 @@ X=onehotencoder.fit_transform(X).toarray()
 labelencoder_y=LabelEncoder()
 y=labelencoder_y.fit_transform(y)
 #splitting the values test and train set
-from sklearn.cross_validation import train_test_split
+from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test=train_test_split(X, y, test_size= 0.2, random_state=0)
 
 #feature scaling
